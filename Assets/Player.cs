cc027933b9a8275fcc2238a8feff808805c9b7b6
Rasmus Riis <rasmus.aagot.riis@gmail.com>
2022-01-27 13:56:15 +0100

    Første holding kode

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,12 +6,21 @@ public class Player : MonoBehaviour
 {
     public float speed;
     Rigidbody2D rb;
+    [Space]
+    public Item Holding = Item.None;
+
+    //Items Player kan holde
+    public enum Item
+    {
+        None, Beer, Food
+    }
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    //Brug denne funktion til movement
     private void FixedUpdate()
     {
         //Basic movement
@@ -19,5 +28,11 @@ public class Player : MonoBehaviour
         
         //Player kigger retningen de bevæger sig
         transform.localScale = new Vector3(Input.GetAxisRaw("Horizontal") != 0 ? Input.GetAxisRaw("Horizontal") : transform.localScale.x, 1, 1);
+    }
+
+    //Brug denne til alt andet
+    private void Update()
+    {
+        
     }
 }
