d87b8184a7a79c255b802398a4991a6aef9d2448
Karl Erik <real.vigamer@hotmail.com>
2022-02-10 10:17:16 +0100

    Tilføjede Goblin walking animation Prøvede på at få goblin til at dreje sig (fejlede) Tilføjede bøf Rettede Goblin sprite

--- Assets/Scripts/GoblinBehaviour.cs
+++ Assets/Scripts/GoblinBehaviour.cs
@@ -6,22 +6,58 @@ public class GoblinBehaviour : MonoBehaviour
 {
     private Transform food;
     [SerializeField] private float speed;
-    public GameObject Chocolate;
+    public GameObject Food;
+    Animator anim;
+    float movement;
+    private bool left, right;
 
     // Start is called before the first frame update
     void Start()
     {
-        Chocolate = GameObject.Find("Chocolate");
-        food = Chocolate.transform;
+        right = true;
+        Food = GameObject.Find("Beef");
+        food = Food.transform;
+        anim = GetComponent<Animator>();
+        anim.SetBool("walking", true);
+        TurnLeft();
     }
 
     // Update is called once per frame
     void Update()
     {
         FollowFood();
+        float movement = 1; //skal være lig med goblins horizontale bevægelse
+        Debug.Log(movement);
+        if (movement < 0)
+        { 
+            TurnLeft();
+        }
+        else if (movement > 0)
+        {        
+            TurnRight();
+        }
+            
+       
     }
     public void FollowFood()
     {
         transform.position = Vector3.MoveTowards(transform.position, food.position, speed * Time.deltaTime);
+        
+    }
+    public void TurnLeft()
+    {
+        if (left)
+            return;
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        left = true;
+        right = false;
+    }
+    public void TurnRight()
+    {
+        if (right)
+            return;
+        transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        left = false;
+        right = true;
     }
 }
